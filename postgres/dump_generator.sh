85d439c7361ff35d5a791bf960ae1925b1541c38
Gonzalo Nahuel Vaca <vacagonzalo@gmail.com>
2020-07-12 13:45:39 -0300

    Correccion de formato

--- postgres/dump_generator.sh
+++ postgres/dump_generator.sh
@@ -4,9 +4,9 @@ CONTAINER=postgres
 USER=docker
 
 printf "=====================================================================\n"
-printf "Contenedor: $CONTAINER"
-printf "Usuario: $USER"
-printf "Nota: se necesita tener el contenedor de postgres corriendo."
+printf "Contenedor: $CONTAINER\n"
+printf "Usuario: $USER\n"
+printf "Nota: se necesita tener el contenedor de postgres corriendo.\n"
 printf "=====================================================================\n"
 
 docker exec -t $CONTAINER pg_dumpall -c -U $USER > dump_`date +%d-%m-%Y"_"%H_%M_%S`.sql
